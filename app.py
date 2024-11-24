3ba34484b856b96f0f751d622066100390d673c1
Bhavadharanisethuraman <22d111@psgitech.ac.in>
2024-11-24 20:03:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ hide_streamlit_style = """
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
+#st.set_option('deprecation.showfileUploaderEncoding', False)
 @st.cache(allow_output_mutation=True)
 def load_model():
     model=tf.keras.models.load_model('tomatoes.h5')
